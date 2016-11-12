6c6531abe045fb94fb19435ab2ebee9793e2964c
panchishin <anton@panchishin.com>
2016-11-12 13:36:52 -0500

    update code to use y_ to mean the ground truth instead of output which is more inline with literature.

--- solutions/03-regularization.py
+++ solutions/03-regularization.py
@@ -5,15 +5,15 @@
 import tensorflow as tf
 
 ### constant data
-x = [[0.,0.],[1.,1.],[1.,0.],[0.,1.]]
-y = [[0.],[0.],[1.],[1.]]
+x  = [[0.,0.],[1.,1.],[1.,0.],[0.,1.]]
+y_ = [[0.],[0.],[1.],[1.]]
 
 ### induction
 # 1x2 input -> 2x3 hidden sigmoid -> 3x1 sigmoid output
 
 # Layer 0 = the x2 inputs
-x0 = tf.constant( x , dtype=tf.float32 )
-y0 = tf.constant( y , dtype=tf.float32 )
+x0 = tf.constant( x  , dtype=tf.float32 )
+y0 = tf.constant( y_ , dtype=tf.float32 )
 
 # Layer 1 = the 2x3 hidden sigmoid
 m1 = tf.Variable( tf.random_uniform( [2,3] , minval=0.1 , maxval=0.9 , dtype=tf.float32  ))
@@ -23,16 +23,16 @@ h1 = tf.sigmoid( tf.matmul( x0,m1 ) + b1 )
 # Layer 2 = the 3x1 sigmoid output
 m2 = tf.Variable( tf.random_uniform( [3,1] , minval=0.1 , maxval=0.9 , dtype=tf.float32  ))
 b2 = tf.Variable( tf.random_uniform( [1] , minval=0.1 , maxval=0.9 , dtype=tf.float32  ))
-y_ = tf.sigmoid( tf.matmul( h1,m2 ) + b2 )
+y_out = tf.sigmoid( tf.matmul( h1,m2 ) + b2 )
 
 
 ### loss
 
-# loss : sum of the squares of y0 - y_
-loss = tf.reduce_sum( tf.square( y0 - y_ ) )
+# loss : sum of the squares of y0 - y_out
+loss = tf.reduce_sum( tf.square( y0 - y_out ) )
 
 # regularize the average square of m1 and m2
-regularization = ( tf.reduce_mean( tf.square( m1 ) ) + tf.reduce_mean( tf.square( m2) ) ) 
+regularization = ( tf.reduce_mean( tf.square( m1 ) ) + tf.reduce_mean( tf.square( m2 ) ) ) 
 
 # training step : gradient decent (1.0) to minimize loss + regularization * 0.01
 train = tf.train.GradientDescentOptimizer(1.0).minimize(loss + regularization*0.01)
@@ -49,8 +49,8 @@ with tf.Session() as sess:
       sess.run(train)
     print sess.run([loss,regularization])
 
-  results = sess.run([m1,b1,m2,b2,y_,loss])
-  labels  = "m1,b1,m2,b2,y_,loss".split(",")
+  results = sess.run([m1,b1,m2,b2,y_out,loss])
+  labels  = "m1,b1,m2,b2,y_out,loss".split(",")
   for label,result in zip(*(labels,results)) :
     print ""
     print label
