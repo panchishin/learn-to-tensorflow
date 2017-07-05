f045daf8f22b7d71b9429b43fcc9ee9dbf645c01
panchishin <anton@panchishin.com>
2017-07-05 11:37:26 -0400

    define local cache for mnist

--- examples/mnist_model_comparison.py
+++ examples/mnist_model_comparison.py
@@ -4,7 +4,7 @@ import time
 
 def get_mnist_data() :
   from tensorflow.examples.tutorials.mnist import input_data
-  return input_data.read_data_sets('/tmp/data', one_hot=True)
+  return input_data.read_data_sets('./cache', one_hot=True)
 
 
 def weight_variable(shape):
