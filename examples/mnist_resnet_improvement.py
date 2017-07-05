3be53a0904de4aed1b5d01382cbb2bd150f2701a
panchishin <anton@panchishin.com>
2017-07-05 12:08:08 -0400

    improve naming

--- examples/mnist_resnet_improvement.py
+++ examples/mnist_resnet_improvement.py
@@ -10,7 +10,7 @@ def parameter_count( shape , name="") :
 
 def weight_variable(shape, name="Weight_Variable"):
   parameter_count(shape,name)
-  return tf.Variable( tf.truncated_normal(shape, stddev=0.01), name=name )
+  return tf.Variable( tf.truncated_normal(shape, stddev=0.01), name=name)
 
 def bias_variable(shape, name="Bias_Variable"):
   return tf.Variable( tf.constant(0.1, shape=shape), name=name)
@@ -22,19 +22,19 @@ def avg_pool(x,stride=2, padding='SAME'):
   return tf.nn.avg_pool(x, ksize=[1, stride, stride, 1], strides=[1, stride, stride, 1], padding=padding)
 
 def conv( x , layers_in , layers_out , width=6 , stride=1, padding='SAME', name="conv" ):
-  w = weight_variable( [width, width, layers_in, layers_out] , name=name) 
+  w = weight_variable( [width, width, layers_in, layers_out], name=(name + "_weight")) 
   b = bias_variable( [layers_out] ) 
-  return tf.nn.conv2d( x, w, strides= [1, stride, stride, 1], padding=padding ) + b
+  return tf.add( tf.nn.conv2d( x, w, strides= [1, stride, stride, 1], padding=padding ) , b , name=name)
 
 def drop_conv( keep_prob, x , layers_in , layers_out , width=6 , stride=1, padding='SAME', name="drop_conv" ):
-  w = weight_variable( [width, width, layers_in, layers_out] , name=name ) 
+  w = weight_variable( [width, width, layers_in, layers_out], name=(name + "_weight") ) 
   w = tf.nn.dropout( w, keep_prob ) / keep_prob
   b = bias_variable( [layers_out] ) 
-  return tf.nn.conv2d( x, w, strides= [1, stride, stride, 1], padding=padding ) + b
+  return tf.add( tf.nn.conv2d( x, w, strides= [1, stride, stride, 1], padding=padding ) , b , name=name)
 
 def conv_relu( x , layers_in , layers_out , width=6 , stride=1, padding='SAME', name="conv_relu" ):
-  h = conv( x , layers_in , layers_out , width , stride, padding, name=name )
-  return tf.nn.relu( h )
+  h = conv( x , layers_in , layers_out , width , stride, padding, name=(name + "_conv") )
+  return tf.nn.relu( h , name=name)
 
 def batch_normalization( x, training, momentum=0.9 ) :
   return tf.layers.batch_normalization( x, training=training, momentum=momentum )
@@ -45,25 +45,25 @@ def single_resnet_block( x, layers, width , training, momentum=0.9, name="single
   return conv( result, layers, layers, width=width, name=name )
 
 def resnet_block( x, layers, width , training, momentum=0.9, name="resnet_block" ) :
-  result = single_resnet_block( x,      layers, width, training, momentum=momentum, name=name )
-  result = single_resnet_block( result, layers, width, training, momentum=momentum, name=name )
-  return tf.add( x , result )
+  result = single_resnet_block( x,      layers, width, training, momentum=momentum, name=(name + "_1") )
+  result = single_resnet_block( result, layers, width, training, momentum=momentum, name=(name + "_2") )
+  return tf.add( x , result, name=name )
 
 def resnet_narrow( x, layers, width , training, narrowing=2, name="resnet_narrow" ) :
   result = batch_normalization( x , training )
   result = tf.nn.relu(result)
-  result = conv( result, layers, layers/narrowing, width=1, name=name )
+  result = conv( result, layers, layers/narrowing, width=1, name=(name + "_narrowing") )
   result = tf.nn.relu(result)
-  result = conv( result, layers/narrowing, layers/narrowing, width=width, name=name )
+  result = conv( result, layers/narrowing, layers/narrowing, width=width, name=(name + "_conv") )
   result = tf.nn.relu(result)
-  result = conv( result, layers/narrowing, layers, width=1, name=name )
+  result = conv( result, layers/narrowing, layers, width=1, name=(name + "_expand") )
   result = tf.nn.relu(result)
-  return tf.add( x , result )
+  return tf.add( x , result, name=name )
 
 def fully_connected( x , size_in , size_out, name="fully_connected" ):
-  W = weight_variable( [size_in, size_out], name=name )
+  W = weight_variable( [size_in, size_out], name=(name + "_weight") )
   b = bias_variable( [size_out] )
-  return tf.matmul(x, W) + b
+  return tf.add( tf.matmul(x, W) , b , name=name )
 
 
 def handler_wrapper( handler ) :
@@ -423,14 +423,13 @@ class conv_pool_with_msra :
 
 def compareModels() :
 
+  print "Long run"
+
   model_list = [ simple0(), simple1(), simple2(), simple3(), simple4(), simple5(),
     simple6(), simple7(), simple8(), simple9(), simple10(), simple11(),
     simple12(), simple13(), simple14(), simple15(), simple16(), simple17(),
     simple18(), simple19() ]
 
-  model_list = []
-
-  print "Long run"
   results = []
 
   for item in model_list :
