46df4e334e5ac4bd9427cca36e81e21805793578
panchishin <anton@panchishin.com>
2017-07-14 15:24:43 -0400

    rewrite using resnet

--- examples/autoencode.py
+++ examples/autoencode.py
@@ -11,38 +11,57 @@ x_reshape = tf.reshape( x0, [-1,SIZE,SIZE,1], name="x_in" )
 x_noisy = layer.high_low_noise( x_reshape , HIGH_LOW_NOISE)
 
 
-def convolution_layers(x_in, layer_sizes=[1,18,24,28], width_sizes=[5,5,4]) :
-  stages = [x_in]
-  for in_size,out_size,width,end in zip(layer_sizes[:-1],layer_sizes[1:],width_sizes,range(len(width_sizes)-1,-1,-1)) :
-    stages.append( layer.conv_relu( stages[-1], in_size, out_size, width=width, padding="VALID" ) )
-    if end != 0 :
-      stages.append( layer.max_pool( stages[-1] ) )
-  return stages
 
-def deconvolution_layers(stages, layer_sizes=[1,18,24,28], width_sizes=[5,5,4]) :
-  for in_size,out_size,width,end in zip(layer_sizes[:-1],layer_sizes[1:],width_sizes,range(len(width_sizes)))[::-1] :
-    stages.append( layer.relu_deconv( stages[-1], out_size, in_size, width=width, shape=tf.shape(stages[end*2]) ) ) 
-    if end != 0 :
-      stages.append( layer.upscaleFlat( stages[-1] , scale=2 ) )
-  return stages
+def convolution_layers( image, training) :
+    stages = []
+    stages.append( layer.batch_normalization( image , training=training ) )
+    stages.append( layer.conv_relu( stages[-1] , 1 , 16 , width=5, padding='VALID' ) )
+    stages.append( layer.resnet_block( stages[-1] , 16, 3 , training=training, momentum=0.99 ) )
+    stages.append( layer.resnet_downscale( stages[-1] ) )
+    stages.append( layer.resnet_block( stages[-1] , 32, 3 , training=training, momentum=0.99 ) )
+    stages.append( layer.resnet_downscale( stages[-1] ) )
+    stages.append( layer.resnet_block( stages[-1] , 64, 3 , training=training, momentum=0.99 ) )
+    stages.append( layer.resnet_downscale( stages[-1] ) )
+    stages.append( layer.resnet_narrow( stages[-1] , 128, 3 , training=training, narrowing=4, momentum=0.99 ) )
+    stages.append( layer.avg_pool( stages[-1] , stride=3 ) )
+    stages.append( layer.conv_relu( stages[-1] , 128 , EMBED_SIZE , width=1, padding='SAME' ) )
+    return stages
 
-def autoencode(x_in, layer_sizes=[1,18,24,28], width_sizes=[5,5,4]) :
+def deconvolution_layers( stages, training) :
+    stages.append( layer.conv_relu( stages[-1] , EMBED_SIZE , 128 , width=1, padding='SAME' ) )
+    stages.append( layer.upscaleFlat( stages[-1] , scale=3 ) )
+    stages.append( layer.resnet_narrow( stages[-1] , 128, 3 , training=training, narrowing=4, momentum=0.99 ) )
+    stages.append( layer.resnet_upscale( stages[-1] ) )
+    stages.append( layer.resnet_block( stages[-1] , 64, 3 , training=training, momentum=0.99 ) )
+    stages.append( layer.resnet_upscale( stages[-1] ) )
+    stages.append( layer.resnet_block( stages[-1] , 32, 3 , training=training, momentum=0.99 ) )
+    stages.append( layer.resnet_upscale( stages[-1] ) )
+    stages.append( layer.resnet_block( stages[-1] , 16, 3 , training=training, momentum=0.99 ) )
+    stages.append( layer.relu_deconv( stages[-1], 16, 1, shape=tf.shape(stages[0]), width=5 ) )
+    stages.append( layer.conv_relu( stages[-1] , 1 , 1 , width=5, padding='SAME' ) )
+    return stages
+
+
+def autoencode(x_in, training) :
   print "==== Convolution ===="
-  stages = convolution_layers(x_in, layer_sizes, width_sizes)
+  stages = convolution_layers(x_in, training=training)
   print "==== Embedding ===="
   embedding = stages[-1]
   print "==== Deconvolution ===="
-  return embedding, deconvolution_layers(stages, layer_sizes, width_sizes)
+  return embedding, deconvolution_layers(stages, training=training)
 
-
-embedding, stages = autoencode( x_noisy , layer_sizes=[1,18,24,EMBED_SIZE], width_sizes=[5,5,4])
+training = tf.placeholder(dtype=tf.bool)
+embedding, stages = autoencode( x_noisy , training=training )
 
 
 x_in = x_reshape
 x_out = tf.nn.relu( stages[-1] , name="x_out" )
 
+update_ops   = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+    
 encode_loss = tf.reduce_mean( tf.reduce_mean( tf.square( x_in - x_out ) ) )
-encode_train = tf.train.AdamOptimizer(1e-3).minimize(encode_loss)
+with tf.control_dependencies(update_ops):
+  encode_train = tf.train.AdamOptimizer(1e-4).minimize(encode_loss)
 
 
 y0 = tf.placeholder(tf.float32, [None, 10], name="y0")
@@ -52,10 +71,12 @@ y_logit = layer.fully_connected( out_fc_1 , EMBED_SIZE , 10 )
 y_out = tf.nn.softmax( y_logit )
 
 classify_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=y_logit, labels=y0))
-classify_train = tf.train.AdamOptimizer(1e-3).minimize(classify_loss)
+with tf.control_dependencies(update_ops):
+  classify_train = tf.train.AdamOptimizer(1e-4).minimize(classify_loss)
 
 total_loss = tf.reduce_sum( tf.log( tf.stack( [encode_loss,classify_loss] ) ) )
-total_train = tf.train.AdamOptimizer(1e-4).minimize(total_loss)
+with tf.control_dependencies(update_ops):
+  total_train = tf.train.AdamOptimizer(1e-4).minimize(total_loss)
 
 
 y_out_index = tf.argmax(y_out,1)
@@ -77,7 +98,7 @@ print """
 ==== Tensor Shapes ====
 """
 
-results = sess.run(stages, feed_dict={x0:mnist.train.next_batch(4)[0]})
+results = sess.run(stages, feed_dict={training:False,x0:mnist.train.next_batch(4)[0]})
 for result,stage in zip(results,stages) :
     print result.shape , stage.name
 
@@ -85,38 +106,37 @@ print """
 =======================
 """
 
+def report(index,batch_correct,result) :
+    if index == 1 or ( index <= 50 and index % 10 == 0 ) or ( index <= 500 and index % 100 == 0 ) or index % 1000 == 0 :
+        correct_result = sess.run( correct, feed_dict={training:False,x0:mnist.test.images,y0:mnist.test.labels})
+        print "index : %5d, loss: %5.3f, train error %4.1f, test error %4.1f percent" % ( index, result, 100*(1 - batch_correct), 100*(1 - correct_result) )
+
 def doAutoEncodeTraining( batches, batch_size=200 ) :
   print "doAutoEncodeTraining"
   for index in range(1,batches+1) :
     batch_x , batch_y = mnist.train.next_batch(batch_size)
-    batch_correct,result,_ = sess.run( [correct,encode_loss,encode_train], feed_dict={x0:batch_x,y0:batch_y})
-    if index == 1 or ( index < 100 and index % 10 == 0 ) or ( index < 1000 and index % 100 == 0 ) or index % 1000 == 0 :
-        correct_result = sess.run( correct, feed_dict={x0:mnist.test.images,y0:mnist.test.labels})
-        print "index : %5d, loss: %5.3f, train error %4.1f, test error %4.1f percent" % ( index, result, 100*(1 - batch_correct), 100*(1 - correct_result) )
+    batch_correct,result,_ = sess.run( [correct,encode_loss,encode_train], feed_dict={training:True,x0:batch_x,y0:batch_y})
+    report(index,batch_correct,result)
 
 def doClassifyTraining( batches, batch_size=200) :
   print "doClassifyTraining"
   for index in range(1,batches+1) :
     batch_x , batch_y = mnist.train.next_batch(batch_size)
-    batch_correct,result,_ = sess.run( [correct,classify_loss,classify_train], feed_dict={x0:batch_x,y0:batch_y})
-    if index == 1 or ( index < 100 and index % 10 == 0 ) or ( index < 1000 and index % 100 == 0 ) or index % 1000 == 0 :
-        correct_result = sess.run( correct, feed_dict={x0:mnist.test.images,y0:mnist.test.labels})
-        print "index : %5d, loss: %5.3f, train error %4.1f, test error %4.1f percent" % ( index, result, 100*(1 - batch_correct), 100*(1 - correct_result) )
+    batch_correct,result,_ = sess.run( [correct,classify_loss,classify_train], feed_dict={training:True,x0:batch_x,y0:batch_y})
+    report(index,batch_correct,result)
 
 def doTotalTraining( batches, batch_size=200) :
   print "doTotalTraining"
   for index in range(1,batches+1) :
     batch_x , batch_y = mnist.train.next_batch(batch_size)
-    batch_correct,result,_ = sess.run( [correct,total_loss,total_train], feed_dict={x0:batch_x,y0:batch_y})
-    if index == 1 or ( index < 100 and index % 10 == 0 ) or ( index < 1000 and index % 100 == 0 ) or index % 1000 == 0 :
-        correct_result = sess.run( correct, feed_dict={x0:mnist.test.images,y0:mnist.test.labels})
-        print "index : %5d, loss: %5.3f, train error %4.1f, test error %4.1f percent" % ( index, result, 100*(1 - batch_correct), 100*(1 - correct_result) )
+    batch_correct,result,_ = sess.run( [correct,total_loss,total_train], feed_dict={training:True,x0:batch_x,y0:batch_y})
+    report(index,batch_correct,result)
 
 
 def showExample(count) :
   for _ in range(count) :
     sample_x,sample_y_in = mnist.train.next_batch(1)
-    sample_in,sample_out,sample_y_out = sess.run([x_in,x_out,y_out],feed_dict={x0:sample_x} )
+    sample_in,sample_out,sample_y_out = sess.run([x_in,x_out,y_out],feed_dict={training:False,x0:sample_x} )
     print"Estimated / Actual class [chance] : ",
     print np.argmax(sample_y_out[0]),"[",int(round(100.0*sample_y_out[0][np.argmax(sample_y_out[0])])),"]",
     print "/",
@@ -128,7 +148,7 @@ def showExample(count) :
 
 
 def confusion() :
-  y_in_result,y_out_result = sess.run( [y_in_index,y_out_index], feed_dict={x0:mnist.test.images,y0:mnist.test.labels})
+  y_in_result,y_out_result = sess.run( [y_in_index,y_out_index], feed_dict={training:False,x0:mnist.test.images,y0:mnist.test.labels})
   for in_index in range(10) :
     print in_index," = ",
     for out_index in range(10) :
@@ -139,8 +159,8 @@ def confusion() :
 
 
 def error_result() :
-  correct_result = sess.run( correct, feed_dict={x0:mnist.test.images,y0:mnist.test.labels})
-  return round(100*(1 - correct_result))
+  correct_result = sess.run( correct, feed_dict={training:False,x0:mnist.test.images,y0:mnist.test.labels})
+  return "There is",100*(1 - correct_result),"percent error"
 
 
 def help() :
@@ -155,3 +175,14 @@ error_result()
 
 help()
 
+showExample(2)
+doAutoEncodeTraining(1000)
+showExample(2)
+doClassifyTraining(1000)
+showExample(2)
+doTotalTraining(1000)
+showExample(2)
+doTotalTraining(5000,500)
+showExample(2)
+confusion()
+error_result()
